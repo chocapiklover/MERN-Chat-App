924d469f5291545796f8361f5ec8f121bdb7cbec
chocapiklover <a.harmuth33@gmail.com>
2024-04-04 13:29:59 +0200

    updated socket url

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -19,7 +19,7 @@ export const SocketContextProvider = ({ children }) => {
 
         //creates new WebSocket connection using io
         if(authUser) {
-            const socket = io('http://localhost:5000',{
+            const socket = io('https://mern-chat-app-ujdb.onrender.com/',{
             // passing the auth userid to the websocket
             query:{
                 userId: authUser._id
